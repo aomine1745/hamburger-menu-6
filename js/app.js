62f17383d508339c083a0b4c72c0557ddd70dd85
aomine <xomarxpl07@outlook.es>
2019-03-12 06:36:13 -0500

    v 1.0

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('.btn-toggle'),
+lines = btn.querySelectorAll('.line');
+let btnClass = 'open';
+
+btn.addEventListener("click", () => {
+	if (btn.classList.contains('open')) {
+		btn.classList.remove(btnClass);
+		btnClass = 'close';
+	} else if (btn.classList.contains('close')) {
+		btn.classList.remove(btnClass);
+		btnClass = 'open';
+	}
+
+	void btn.offsetWidth;
+	btn.classList.add(btnClass);
+});
